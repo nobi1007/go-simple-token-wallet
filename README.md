c4bb9476e7822ed2c0c0386381fdf686b06482e1
nobi1007 <mittalshyam1007@gmail.com>
2024-09-10 19:30:01 +0530

    add cli for querying balances

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 2. Retrieve the wallet balance of a specific ERC-20 token (e.g., DAI or USDT).
 3. Send tokens from one wallet to another.
 4. Display transaction history.
+
+```bash
+# Run following to query the user's uniETH balance
+
+go run main.go uniethBalance -z API_KEY -a WALLET_ADDRESS
+```
--- main.go
+++ main.go
@@ -6,29 +6,36 @@ import (
 	"log"
 	"math"
 	"math/big"
+	"os"
+
 	uniETHToken "simple-token-wallet/token"
 	"simple-token-wallet/util"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+
+	"github.com/urfave/cli/v2"
 )
 
-func getBalance(client *ethclient.Client, addr string) (*big.Int, *big.Float) {
+func printBalance(balance *big.Int, balanceType string) {
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethBalance := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Printf("%v Balance: %v wei (%v %v)\n", balanceType, balance, ethBalance, balanceType)
+}
+
+func getBalance(client *ethclient.Client, addr string) *big.Int {
 	accountAddress := common.HexToAddress(addr)
 	balance, err := client.BalanceAt(context.Background(), accountAddress, nil)
 	if err != nil {
 		log.Fatalf("Error getting balance: %v", err)
 	}
 
-	fbalance := new(big.Float)
-	fbalance.SetString(balance.String())
-	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18))).SetPrec(8)
-
-	return balance, ethValue
+	return balance
 }
 
-func getUniETHBalance(client *ethclient.Client, tokenAddr string, addr string) (balance *big.Int, ethBalance *big.Float) {
+func getUniETHBalance(client *ethclient.Client, tokenAddr string, addr string) (balance *big.Int) {
 	tokenAddress := common.HexToAddress(tokenAddr)
 	accountAddress := common.HexToAddress(addr)
 
@@ -41,9 +48,6 @@ func getUniETHBalance(client *ethclient.Client, tokenAddr string, addr string) (
 	if err != nil {
 		log.Fatalf("Failed to load the uniETH balance: %v\n", err)
 	}
-	fbalance := new(big.Float)
-	fbalance.SetString(balance.String())
-	ethBalance = new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
 	return
 }
 
@@ -57,24 +61,104 @@ func getChainID(client *ethclient.Client) *big.Int {
 
 func main() {
 
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatalf("Failed to load config: %v\n", err)
+	app := &cli.App{
+		Name:  "Simple Token Wallet",
+		Usage: "A simple CLI wallet for balance and sending tokens",
+		Commands: []*cli.Command{
+			{
+				Name:  "balance",
+				Usage: "Check balance for a given address",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "address",
+						Aliases:  []string{"a"},
+						Usage:    "The wallet `ADDRESS` to check the balance of",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "apiKey",
+						Aliases:  []string{"z"},
+						Usage:    "Access Node `ApiKey` can be retrieved from https://access.rockx.com",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					address := c.String("address")
+					apiKey := c.String("apiKey")
+
+					client, err := ethclient.Dial(fmt.Sprintf("https://eth.w3node.com/%v/api", apiKey))
+
+					if err != nil {
+						log.Fatalf("Failed to connect to eth client: %v", err)
+					}
+					printBalance(getBalance(client, address), "ETH")
+					return nil
+				},
+			},
+			{
+				Name:  "uniethBalance",
+				Usage: "Checks the uniETH balance for the given address",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "address",
+						Aliases:  []string{"a"},
+						Usage:    "The wallet `Address` to check the balance of",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "apikey",
+						Aliases:  []string{"z"},
+						Usage:    "Access Node `ApiKey` can be retrieved from https://access.rockx.com",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					address := c.String("address")
+					apiKey := c.String("apikey")
+
+					config, err := util.LoadConfig(".")
+					if err != nil {
+						log.Fatalf("Failed to load config: %v", err)
+					}
+
+					client, err := ethclient.Dial(fmt.Sprintf("https://eth.w3node.com/%v/api", apiKey))
+					if err != nil {
+						log.Fatalf("Failed to connect to eth client: %v", err)
+					}
+					printBalance(getUniETHBalance(client, config.UniethTokenAddress, address), "uniETH")
+					return nil
+				},
+			},
+			{
+				Name:  "chainID",
+				Usage: "Returns the chain ID",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "apikey",
+						Aliases:  []string{"z"},
+						Usage:    "Access Node `ApiKey` can be retrieved from https://access.rockx.com",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					apiKey := c.String("apikey")
+
+					client, err := ethclient.Dial(fmt.Sprintf("https://eth.w3node.com/%v/api", apiKey))
+					if err != nil {
+						log.Fatalf("Failed to connect to eth client: %v", err)
+					}
+
+					chainID := getChainID(client)
+					fmt.Printf("Chain ID: %v\n", chainID)
+
+					return nil
+				},
+			},
+		},
 	}
 
-	client, err := ethclient.Dial(fmt.Sprintf("https://eth.w3node.com/%v/api", config.EthClientAPIKey))
+	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatalf("Failed to connect to eth client: %v", err)
+		log.Fatal(err)
 	}
-	fmt.Println("Successfully connected to eth client")
-
-	fmt.Printf("Chain ID: %v\n", getChainID(client))
-
-	weiBalance, ethBalance := getBalance(client, config.UserAddress)
-
-	fmt.Printf("ETH Balance: %v wei (%v ETH)\n", weiBalance, ethBalance)
-
-	uniEthWeiBalance, uniEthBalance := getUniETHBalance(client, config.UniethTokenAddress, config.UserAddress)
-
-	fmt.Printf("uniETH Balance: %v wei (%v uniETH)\n", uniEthWeiBalance, uniEthBalance)
 }
